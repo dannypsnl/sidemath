b258594d3b146321d5e58834587c71b09b9f560c
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-08-29 21:18:53 +0800

    open a panel and have a mathlive input block in it

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,28 +9,66 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "sidemath" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sidemath" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "sidemath.open",
+    function () {
+      vscode.window.showInformationMessage("edit your formula in sidemath");
+      const panel = vscode.window.createWebviewPanel(
+        "sidemath",
+        "Math on your side",
+        vscode.ViewColumn.Beside,
+        {
+          enableScripts: true,
+        }
+      );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('sidemath.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      panel.webview.html = getWebviewContent();
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from sidemath!');
-	});
+      panel.onDidDispose(
+        () => {
+          panel = undefined;
+        },
+        null,
+        context.subscriptions
+      );
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
+}
+
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script defer src="https://cdn.jsdelivr.net/npm/mathlive"></script>
+  <title>Edit Formula</title>
+</head>
+<body>
+	<math-field style="font-size: 2em; width: 100%">x^2+y^2=1</math-field>
+	<script>
+    mf.addEventListener('input', evt =>
+			// This line won't work, but I don't know the reason yet
+			navigator.clipboard.writeText(evt.target.value);
+    );
+  </script>
+</body>
+</html>`;
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
